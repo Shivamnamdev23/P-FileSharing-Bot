ac4b70d04b78eed76f6772a383bd10e1b5968bcf
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2024-04-03 04:31:48 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -205,8 +205,8 @@ async def start_command(client: Client, message: Message):
                 copied_messages.append(f)
             except:
                 pass
-        k = await client.send_message(chat_id=message.from_user.id, text="<b>This video/file will be deleted in 10 minutes (Due to Pornography  issues).\n\n📌 Please forward this video/file to somewhere else and start downloading there.</b>")
-        await asyncio.sleep(600)
+        k = await client.send_message(chat_id=message.from_user.id, text="<b>This video/file will be deleted in 30 minutes (Due to Pornography  issues).\n\n📌 Please forward this video/file to somewhere else and start downloading there.</b>")
+        await asyncio.sleep(1800)
         for f in copied_messages:
             await f.delete()
         await k.edit_text("Your video is successfully deleted!")
