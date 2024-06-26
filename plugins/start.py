d8c26d2653725d55d8bb9476feb0103cd2e32167
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2024-06-26 00:42:01 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,7 +13,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 from datetime import datetime
 
 # 1 minutes = 60, 2 minutes = 60×2=120, 5 minutes = 60×5=300
-SECONDS = int(os.getenv("SECONDS", "10"))
+SECONDS = int(os.getenv("SECONDS", "1800"))
 
 
 @Bot.on_message(filters.command("start") & filters.private)
@@ -87,7 +87,7 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
             
-        k = await client.send_message(chat_id=message.from_user.id, text="<b>This video/file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video/file to somewhere else and start downloading there.</b>")
+        k = await client.send_message(chat_id=message.from_user.id, text="<b>This video/file will be deleted in 30 minutes (Due to copyright issues).\n\n📌 Please forward this video/file to somewhere else and start downloading there.</b>")
         await asyncio.sleep(600)
         for f in copied_messages:
             await f.delete()
