1da7b143e79c8bf8fc9473216685c46551167b08
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2024-07-01 07:16:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ except ValueError:
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previouscaption}\n\n♻️ Upload By [ @KoreandramaCinema ]</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previouscaption}\n\n♻️ Upload By [ @snfilmy ]</b>")
 
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
